df774b953e05237dae57590acb568c3659bb1f75
Patriciu Lucian Nista Oltean <subgeo98@gmail.com>
2016-10-07 00:11:30 +0200

    LEDs, libraries and more
    *Changed "ifaddr" library for using "netifaces" because works with python3 and "ifaddr" doesn't.
    
    *Refused to import the library "gpiozero" because of many errors & delays. Also, when returning from another program, the button values still active and unchangable, so I preferred to implement the clasical library of RPi.GPIO.
    
    *Added a function for button 3: calling the program_running() function for knowing you are in the main program.

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #Llibreries
 import os
-import ifaddr
+import netifaces
 import time
 import RPi.GPIO as GPIO
-from gpiozero import LED, Button
 
 #Execucions previes
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 #Variables globals
 BUTTON1 = 6
 BUTTON2 = 5
 BUTTON3 = 25
 BUTTON4 = 24
+LED1 = 23
+LED2 = 18
+LED3 = 17
 APON = 0
 ETHON = 0
-adapters = ifaddr.get_adapters()
+adapter = netifaces.interfaces()
 INTFC = "eth0"
 apretat = 0
 seconds = 0.5
@@ -27,19 +30,37 @@ GPIO.setup(BUTTON1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(BUTTON4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
+GPIO.setup(LED3, GPIO.OUT)
 
-#Funcio principal per a comprovar la connexio, basada en si tenim una IP assignada.
-def adapter_check():
+#Funcions
+def program_running():
+    """ Funcio que mostra de manera visual que ens trobem en el programa principal """
+    val = True
+    for i in range(2):
+        """ Encen i apaga els leds segons la variable val """
+        GPIO.output(LED1, val)
+        GPIO.output(LED2, val)
+        GPIO.output(LED3, val)
+        ts(2)
+        if val:
+            val = False
+        elif not val:
+            val = True
+
+def adapter_check(): #Funcio principal per a comprovar la connexio, basada en si tenim una IP assignada.
+    """ Comprova en la llista de adaptadors disponibles si l'adaptador que ens interesa te una IP """
     global ETHON
-    """ Comprova si l'adaptador te una IP """
-    for adapter in adapters:
-    	if adapter.name == INTFC:
-            print("L'adaptador esta online i te assignada la IP {}".format(adapter.ips[0].ip))
-            ETHON = 1
-	else:
-	    pass
-
-adapter_check()
+    for a in adapter:
+        if a == INTFC:
+            if netifaces.ifaddresses(INTFC)[2][0]["addr"]:
+                print("L'adaptador d'internet esta online i te assignada la IP {}".format(netifaces.ifaddresses(INTFC)[2][0]["addr"]))
+                ETHON = 1
+            else:
+                print("L'adaptador d'internet no esta online. Crearem un fitxer per a guardar les dades.")
+        else:
+            pass
 
 def ts(temps):
     """ Retorna una pausa del temps especificat en segons """
@@ -48,49 +69,60 @@ def ts(temps):
 def enter_program(number):
     """ Entra en el programa corresponent al boto donant el numero i si no ens trobem dins de un altre programa """
     if (number == 1):
-	print(ETHON)
-	os.system("python program.py {0}".format(ETHON))	
-	ts(seconds)
-	pre_boto()
+        print(ETHON)
+        os.system("python3 program.py {0}".format(ETHON))	
+        ts(seconds)
+        program_running()
+        pre_boto()
 
     elif (number == 2):
-	os.system("python tester.py")
-	ts(seconds)
-	pre_boto()
+        os.system("python3 tester.py")
+        ts(seconds)
+        program_running()
+        pre_boto()
 
     elif (number == 3):
-	pass
+        program_running()
+        ts(seconds)
+        pre_boto()
 
     elif number == 4:
-	exit()
+        GPIO.cleanup()
+        exit()
 
 def pre_boto():
-    print("Premi un boto per a executar el programa que vulgui")
+    """ Funcio que ens posa en un estat d'espera per a que l'usuari esculli el que vol fer. """
+    print("\nPremi un boto per a executar el programa que vulgui")
     while True:
         """ Loop que comprova si un usuari ha apretat algun boto i executa la funcio que obrira el programa corresponent """ 
-        button_1 = GPIO.input(6)
-	button_2 = GPIO.input(5)
-	button_3 = GPIO.input(25)
-	button_4 = GPIO.input(24)
+        button_1 = GPIO.input(BUTTON1)
+        button_2 = GPIO.input(BUTTON2)
+        button_3 = GPIO.input(BUTTON3)
+        button_4 = GPIO.input(BUTTON4)
 	
         #Comprovem quin es el boto que ha apretat l'usuari i executem la funcio que obria el programa.
 	#print(button_1, button_2, button_3, button_4)	
         if button_1 == apretat:
-	    ts(seconds)
-	    enter_program(1)
+            ts(seconds)
+            enter_program(1)
 
         if button_2 == apretat:
-	    ts(seconds)
-    	    enter_program(2)
+            ts(seconds)
+            enter_program(2)
 
         if button_3 == apretat:
-	    #ts(seconds)
-	    #enter_program(3)
-	    pass
+            ts(seconds)
+            enter_program(3)
+            #pass
 
         if button_4 == apretat:
-	    ts(seconds)
-	    enter_program(4)
+            ts(seconds)
+            enter_program(4)
 	
 
+#####################
+# Inici del programa
+#####################
+program_running()
+adapter_check()
 pre_boto()
